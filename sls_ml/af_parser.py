5568c983d33aac526077ffd77da2b03b5ee6c01c
Konrad Drees <konrad.drees@posteo.de>
2023-03-22 16:46:27 +0100

    Update af_parser.py
    uses regex instead. manual slicing seems to have problems

--- sls_ml/af_parser.py
+++ sls_ml/af_parser.py
@@ -1,19 +1,25 @@
+import re
 import networkx as nx
+
 
 
 # A simple parser for the APX file format
 def parse_apx(file_path):
-    af = nx.DiGraph()
+    graph = nx.DiGraph()
+    arg_pattern = re.compile(r'arg\((.+)\)')
+    att_pattern = re.compile(r'att\((.+),\s*(.+)\)')
 
-    with open(file_path, 'r') as f:
-        for line in f:
+    with open(file_path, 'r') as file:
+        for line in file:
             line = line.strip()
-            if line.startswith('arg('):
-                node = line[4:-1]
-                af.add_node(node)
-            elif line.startswith('att('):
-                attacker, attacked = line[4:-1].split(',')
-                af.add_edge(attacker, attacked)
+            arg_match = arg_pattern.match(line)
+            att_match = att_pattern.match(line)
 
-    return af
+            if arg_match:
+                arg_name = arg_match.group(1)
+                graph.add_node(arg_name)
+            elif att_match:
+                arg1, arg2 = att_match.group(1), att_match.group(2)
+                graph.add_edge(arg1, arg2)
 
+    return graph
